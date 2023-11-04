a9c2f5f3913b5c6de3675306a845cd9d852428b0
hellokev <2kevinloi8@gmail.com>
2023-11-04 14:19:39 -0700

    prints top 5 users on leaderboard

--- Api/redPy.py
+++ Api/redPy.py
@@ -1,7 +1,8 @@
 import redis
-
-r = redis.Redis()
-r.mset({"Croatia": "Zagreb", "Bahamas": "Nassau"})
-result = r.get("Bahamas")
-
-print(result.decode('utf-8'))
+r = redis.Redis(host='localhost', port=6379)
+leaderboard_key = 'players'
+top_users = r.zrevrange(leaderboard_key, 0, 4, withscores=True)
+for rank, user in enumerate(top_users, start=1):
+    username = user[0].decode('utf-8')
+    score = user[1]
+    print(f"Rank {rank}: {username} - Score: {score}")
